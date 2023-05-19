8ff6ec799a4a524499db5eba81609a2d438f47da
nishchaybhuker20 <nishchaybhuker123@gmail.com>
2023-05-19 16:03:33 +0530

    fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request
+from typing import Optional
+from fastapi import FastAPI, Request, Header
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -7,7 +8,7 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/index/", response_class=HTMLResponse)
-def index(request: Request):
+def index(request: Request, hx_request: Optional[str] = Header(None)): #hx-request
     films=[
         {"name": "RRR","director":"SS Rajamouli","year":"2021"},
         {"name": "KGF","director":"Prashanth Neel","year":"2018"},
@@ -15,4 +16,7 @@ def index(request: Request):
     ]
 
     context = {"request": request,'films':films}
+    if hx_request:
+        return templates.TemplateResponse("table.html", context)
+    
     return templates.TemplateResponse("index.html", context)   
