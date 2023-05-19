f8ceddfa7a4aa4c9d6128ab9f7831f04bdc6f9e9
nishchaybhuker20 <nishchaybhuker123@gmail.com>
2023-05-19 15:49:45 +0530

    feat

--- main.py
+++ main.py
@@ -8,5 +8,11 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/index/", response_class=HTMLResponse)
 def index(request: Request):
-    context = {"request": request}
+    films=[
+        {"name": "RRR","director":"SS Rajamouli","year":"2021"},
+        {"name": "KGF","director":"Prashanth Neel","year":"2018"},
+        {"name": "Bahubali","director":"SS Rajamouli","year":"2015"},
+    ]
+
+    context = {"request": request,'films':films}
     return templates.TemplateResponse("index.html", context)   
